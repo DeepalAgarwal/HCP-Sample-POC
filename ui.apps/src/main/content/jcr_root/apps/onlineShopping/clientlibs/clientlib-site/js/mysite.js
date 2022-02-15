50ab681a559ce9b9b9773a37c09d8f429f94c237
DeepalAgarwal <deepalagarwal@deloitte.com>
2022-02-15 12:13:28 +0530

    Clientlib site

---
+++ ui.apps/src/main/content/jcr_root/apps/onlineShopping/clientlibs/clientlib-site/js/mysite.js
@@ -0,0 +1,28 @@
+var Constants = {
+        contenttypefilter: "pediatric",
+    };
+/*
+function setCookie(cname, cvalue, exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  let expires = "expires="+ d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+*/
+
+function setCookie(key, value) {
+    var expires = new Date();
+    expires.setTime(expires.getTime() + 31536000000); //1 year  
+    document.cookie = key + '=' + value + ';expires=' + expires.toUTCString();
+}
+
+function getCookie(name) {
+        var cookieArr = document.cookie.split(";");
+        for(var i = 0; i < cookieArr.length; i++) {
+            var cookiePair = cookieArr[i].split("=");
+            if(name == cookiePair[0].trim()) {            
+                return decodeURIComponent(cookiePair[1]);
+            }
+        }
+        return null;
+    }
